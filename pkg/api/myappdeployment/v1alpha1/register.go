9331eef2964eef6d5accbdfca29fd2cc3ab1b8d8
Mangaal <angommeeteimangaal@gmail.com>
2023-10-13 21:53:21 +0530

    code generated

--- pkg/api/myappdeployment/v1alpha1/register.go
+++ pkg/api/myappdeployment/v1alpha1/register.go
@@ -1,33 +1,34 @@
 package v1alpha1
 
 import (
-	"sample-controller-k8s/pkg/api/myappdeployment"
+	myappdeployment "sample-controller-k8s/pkg/api/MyAppDeployment"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 )
 
-var SchemeGroupVesrion = schema.GroupVersion{Group: myappdeployment.GroupName, Version: "v1aplha1"}
+var SchemeGroupVersion = schema.GroupVersion{Group: myappdeployment.GroupName, Version: "v1aplha1"}
 
 func Kind(kind string) schema.GroupKind {
-	return SchemeGroupVesrion.WithKind(kind).GroupKind()
+	return SchemeGroupVersion.WithKind(kind).GroupKind()
 }
 
 func Resource(resource string) schema.GroupResource {
-	return SchemeGroupVesrion.WithResource(resource).GroupResource()
+	return SchemeGroupVersion.WithResource(resource).GroupResource()
 }
 
 var (
-	SchemeBuiler = runtime.NewSchemeBuilder()
+	SchemeBuilder = runtime.NewSchemeBuilder()
+	AddToScheme   = SchemeBuilder.AddToScheme
 )
 
 func addKnownTypes(sheme *runtime.Scheme) error {
-	sheme.AddKnownTypes(SchemeGroupVesrion,
+	sheme.AddKnownTypes(SchemeGroupVersion,
 		&MyAppDeployment{},
 		&MyAppDeploymentList{},
 	)
-	metav1.AddToGroupVersion(sheme, SchemeGroupVesrion)
+	metav1.AddToGroupVersion(sheme, SchemeGroupVersion)
 
 	return nil
 
