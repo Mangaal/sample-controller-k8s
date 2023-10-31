db23976ca8459357eb887ea47a4e66efa359c73c
Mangaal <angommeeteimangaal@gmail.com>
2023-10-31 16:46:54 +0530

    commit

--- main.go
+++ main.go
@@ -3,15 +3,14 @@ package main
 import (
 	"fmt"
 
-	clinetset "sample-controller-k8s/pkg/clinet/clientset/versioned"
-	informer "sample-controller-k8s/pkg/clinet/informers/externalversions"
+	clinetset "sample-controller-k8s/pkg/client/clientset/versioned"
+	informer "sample-controller-k8s/pkg/client/informers/externalversions"
 	"time"
 
 	kubeinformers "k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"k8s.io/klog/v2"
 	"sigs.k8s.io/controller-runtime/pkg/manager/signals"
 )
 
@@ -53,21 +52,7 @@ func main() {
 	kubeShareInformerFactort := kubeinformers.NewSharedInformerFactory(kubernetesClinet, 30*time.Second)
 	sampleInformerFactory := informer.NewSharedInformerFactory(sampleClinetSet, 30*time.Second)
 
-	controller := NewController(ctx,
-		kubernetesClinet,
-		sampleClinetSet,
-		kubeShareInformerFactort.Apps().V1().Deployments(),
-		sampleInformerFactory.Nextgen().V1alpha1().MyAppDeployments(),
-	)
-
 	kubeShareInformerFactort.Start(ctx.Done())
 	sampleInformerFactory.Start(ctx.Done())
 
-	if err := controller.Run(ctx, 2); err != nil {
-
-		fmt.Println("Error running Controller: ", err)
-
-		klog.FlushAndExit(klog.ExitFlushTimeout, 1)
-
-	}
 }
